336191780c6e7c52684cd33f16778fac2709e1ed
Sarah Joy Calpo <sajoy.pc@gmail.com>
2015-02-06 14:31:01 -0800

    make db:seed work!

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,3 @@
-
 Category.create(name: "lesson")
 Category.create(name: "project")
 Category.create(name: "class")
-
-# rake db:seed doesn't work, rcv error:
-# rake aborted!
-# NameError: uninitialized constant Category
-
-# research says something about changing threadsafe! ?
-# http://hightechsorcery.com/2013/01/fixing-uninitialized-constant-errors-in-rake-tasks-when-using-rails-threadsafe-mode/
